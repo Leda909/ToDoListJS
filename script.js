a9ea35328db940b9a1ba3759d7d2182210b7f145
Leda909 <ko9zad@gmail.com>
2024-08-25 10:47:39 +0100

    html + js + css

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+const taskForm = document.getElementById('taskForm');
+const taskInput = document.getElementById('taskInput');
+const taskList = document.getElementById("taskList");
 
+function addTask(){
+    const task =document.createElement("li");
+    task.classList.add("task")
+    const taskCheckbox = document.createElement("input");
+    const taskContent = document.createElement("input");
+    const taskDeleteButton = document.createElement("button");
+}
