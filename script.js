f91d4635c3bfe36032bd21452ad0320eded227ca
Leda909 <ko9zad@gmail.com>
2024-08-26 12:44:39 +0100

    addTask function

--- script.js
+++ script.js
@@ -4,8 +4,21 @@ const taskList = document.getElementById("taskList");
 
 function addTask(){
     const task =document.createElement("li");
-    task.classList.add("task")
+    task.classList.add("task");
+
     const taskCheckbox = document.createElement("input");
+    taskCheckbox.type = "checkbox";
+
     const taskContent = document.createElement("input");
+    taskContent.type = "text";
+    taskContent.value = taskInput.value;
+
     const taskDeleteButton = document.createElement("button");
+    taskDeleteButton.textContent = "x";
+
+    task.appendChild(taskCheckbox);
+    task.appendChild(taskContent);
+    task.appendChild(taskDeleteButton);
+
+    taskList.appendChild(task);
 }
